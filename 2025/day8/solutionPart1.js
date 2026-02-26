d98d92b62ea260fd805ad9032cc4a4544430d2bb
zakgc <zakgriffithcrowe@gmail.com>
2026-02-26 13:21:37 +0000

    part 1 solution 2

--- 2025/day8/solutionPart1.js
+++ 2025/day8/solutionPart1.js
@@ -43,3 +43,89 @@ const findShortest = (startingPoint) => {
     results.sort((a,b) => a.distance - b.distance)
     return results[0]
 }
+
+const indexOfTargetInCircuits = (target) => {
+    let targetIndex
+    circuits.forEach((circuit, index) => {
+        if (circuit.includes(target)) {
+            targetIndex = index
+        }
+    })
+
+    return targetIndex
+}
+
+const moveClosest = (indexA, indexB) => {
+    circuits[indexA] = circuits[indexA].concat(circuits[indexB])
+
+    circuits.splice(indexB, 1)
+}
+
+let circuits = boxPositions.map(boxPosition => [boxPosition])
+
+for (let i = 0; i < boxPositions.length; i++) {
+    let closestBox = findShortest(boxPositions[i]).boxPosition
+
+    let indexOfBox = indexOfTargetInCircuits(boxPositions[i])
+    let indexOfClosest = indexOfTargetInCircuits(closestBox)
+
+    if (indexOfBox === indexOfClosest) continue
+    moveClosest(indexOfBox, indexOfClosest)
+}
+
+
+// const includesBoth = (array, targets) => {
+//     for (let i = 0; i < array.length; i++) {
+//         let result = array[i].every(ele => targets.includes(ele))
+
+//         if (result) return true
+//     }
+
+//     return false
+// }
+
+// const includesOne = (array, target) => {
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i].includes(target)) return true
+//     }
+
+//     return false
+// }
+
+// let circuits = []
+// let removedBoxes = []
+
+// boxPositions.forEach(boxPosition => {
+//     let closestBox = findShortest(boxPosition).boxPosition
+//     if (includesBoth(circuits, [boxPosition, closestBox])) return
+//     if (includesOne(circuits, closestBox)) {
+//         circuits.forEach((circuit) => {
+//             if (circuit.includes(closestBox)) {
+//                 circuit.push(boxPosition)
+
+//                 return
+//             }
+//         })
+//     }
+
+//     circuits.push([
+//         boxPosition, closestBox
+//     ])
+//     removedBoxes.push(closestBox)
+// })
+
+
+const calculateResult = (circuits) => {
+    let lengthOfCircuits = circuits.map(circuit => circuit.length)
+    let sorted = lengthOfCircuits.sort((a, b) => b - a)
+    
+    return {
+        all: sorted,
+        top: sorted.slice(0, 3),
+        answer: sorted[0] * sorted[1] * sorted[2]
+    }
+}
+
+console.log(circuits);
+console.log(circuits.length);
+console.log(calculateResult(circuits));
